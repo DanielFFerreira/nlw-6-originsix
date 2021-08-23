3eb1df9a0da7003c4e98990ccadb7e7eed6be2c0
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-08-23 19:15:40 -0300

    carousel and scrooll swiper function

--- src/main.js
+++ src/main.js
@@ -43,3 +43,23 @@ const swiper = new Swiper('.swiper-container', {
     }
   }
 })
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  #ourclients .title, ul, img
+  footer .brand, footer .social
+  `,
+  { Interval: 100 }
+)
