1f57a554419b1cf0420befe804c63d41d1fbdf6b
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-08-30 15:30:19 -0300

    New scrool comments and apply slick and properties

--- src/main.js
+++ src/main.js
@@ -19,14 +19,15 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', () => {
-  if (window.screenY >= navHeight) {
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
-  }
-  else {
+  } else {
+    // menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -58,8 +59,70 @@ scrollReveal.reveal(
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links,
-  #ourclients .title, ul, img
+  #ourclients .title, img
   footer .brand, footer .social
   `,
   { Interval: 100 }
 )
+
+// Slick
+$(function() {
+  $('.slide-clients').slick({
+    ots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    autoplay: true,
+    autoplaySpeed: 2800,
+    prevArrow: $('#arrow-prev'),
+    nextArrow: $('#arrow-next'),
+    responsive: [
+      {
+        breakpoint: 811,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true
+        }
+      },
+
+      {
+        breakpoint: 430,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  });
+})
+
+// back up button
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  
+  if (window,scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  }
+  else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
